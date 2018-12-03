a38e3fe7870db2e6c5bbd9cb2a9271c8e5c3a7d6
duxun93 <1850175167@qq.com>
2018-12-03 17:07:29 +0800

    12/3

--- Leetcode_easy/20. Valid Parentheses/20_Valid Parentheses Variant.py
+++ Leetcode_easy/20. Valid Parentheses/20_Valid Parentheses Variant.py
@@ -1,2 +1,26 @@
-#if we have str like this :{[()]}{[]}
+#if we have str like this :{[()]}{[]},{[(]}{[)]}
 # how to deal?
+def isValid(s):
+    dic = {'{':'}','[':']','(':')'}
+    L = []
+    P = []
+    M = []
+    for i in s :
+        if dic.__contains__(i):
+            L.append(dic[i])
+        else :
+            P.append(i)
+        if len(L) == len(P):
+            M.append(L)
+            M.append(P)
+            P = 0
+            L = 0
+    for i in range(0,len(M)-1,2) :
+        if M[i] == M[i+1]: 
+            if i+2 == len(M):
+                print('true')
+            else :
+                continue 
+        else :
+            print('false')
+            break 
