59e1751c0e60b370bd92f81cec47d493f246a298
duxun93 <1850175167@qq.com>
2018-12-10 15:36:20 +0800

    12/10

---
+++ Leetcode_easy/38. Count and Say/38_Count and Say.py
@@ -0,0 +1,27 @@
+#https://leetcode.com/problems/count-and-say/
+#用递归来写，类似于数列an = f(an-1)的模式。
+def temp(x):
+    j = 0
+    tmp = x[0]
+    s = ''
+    for i in range(0,len(x)+1) :#range()不包括stop点，range（0,5），输出的是01234五个数，没有5.
+        if i <= len(x)-1 :
+            if x[i] == tmp :
+                j = j + 1
+            else:
+                s = s + str(j) + tmp 
+                tmp = x[i]
+                j = 1
+        else :
+            s = s + str(j) + tmp
+    return s 
+
+def countAndSay(n):
+    p = ''
+    if n == 1 :
+        return '1'
+    else :
+        L = countAndSay(n-1)
+        p = str(temp(L)) 
+        return p
+print(countAndSay(4))
